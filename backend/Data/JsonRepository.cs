83e2d3e3f4d3e727d592d0b13fc917ba529271ed
Christian Benedict Gonzales <chgonzales@fullscale.io>
2023-02-13 13:59:17 +0800

    Add jsonrepository and contacts.json

---
+++ backend/Data/JsonRepository.cs
@@ -0,0 +1,58 @@
+﻿using API.Models;
+using System.Text.Json;
+
+namespace API.Data
+{
+    public class JsonRepository
+    {
+        public List<Contact> Contacts { get; set; }
+
+        // Create a repository that reads contents inside contacts.json and stores it on Contacts property
+        public JsonRepository()
+        {
+            var json = File.ReadAllText("contacts.json");
+            Contacts = JsonSerializer.Deserialize<List<Contact>>(json);
+        }
+
+        // CRUD operations
+
+        // Create a method get all contacts
+        public List<Contact> GetAllContacts()
+        {
+            return Contacts;
+        }
+
+        // create a method to get a contact
+        public Contact GetContact(Guid id)
+        {
+            return Contacts.FirstOrDefault(c => c.Id == id);
+        }
+
+        // create a method to add a contact
+        public void AddContact(Contact contact)
+        {
+            Contacts.Add(contact);
+        }
+
+        // create a method to update contact
+        public void UpdateContact(Contact contact)
+        {
+            var index = Contacts.FindIndex(c => c.Id == contact.Id);
+            Contacts[index] = contact;
+        }
+
+        // create a method to delete a contact
+        public void DeleteContact(Guid id)
+        {
+            var index = Contacts.FindIndex(c => c.Id == id);
+            Contacts.RemoveAt(index);
+        }
+
+        // create a method to save changes
+        public void SaveChanges()
+        {
+            var json = JsonSerializer.Serialize(Contacts);
+            File.WriteAllText("contacts.json", json);
+        }
+    }
+}
