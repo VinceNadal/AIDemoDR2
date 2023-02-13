6eba22b50c21bd0efb7c9dcdd1be95374ae46d65
Christian Benedict Gonzales <chgonzales@fullscale.io>
2023-02-13 14:27:43 +0800

    Complete backend web api

---
+++ backend/Controllers/ContactsController.cs
@@ -0,0 +1,60 @@
+﻿using API.DTOs;
+using API.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactsController : ControllerBase
+    {
+        // Inject JsonContactService
+        private readonly IContactService _contactService;
+
+        public ContactsController(IContactService contactService)
+        {
+            _contactService = contactService;
+        }
+
+        // GET api/contacts
+        [HttpGet]
+        public ActionResult GetAllContacts()
+        {
+            var contacts = _contactService.GetAllContacts();
+            return Ok(contacts);
+        }
+
+        // GET api/contacts/{id}   
+        [HttpGet("{id}")]
+        public ActionResult GetContact(Guid id)
+        {
+            var contact = _contactService.GetContact(id);
+            return Ok(contact);
+        }
+
+        // Create a create contact http post request handler
+        // POST api/contacts
+        [HttpPost]
+        public ActionResult Post(CreateContactDTO contactDTO)
+        {
+            var contact = _contactService.AddContact(contactDTO);
+            return CreatedAtAction(nameof(GetContact), new { id = contact.Id }, contact);
+        }
+
+        // Create an update contact http put request handler
+        [HttpPut("{id}")]
+        public ActionResult Put(Guid id, UpdateContactDTO updateContactDTO)
+        {
+            var contact = _contactService.UpdateContact(updateContactDTO);
+            return NoContent();
+        }
+
+        // Create a delete contact http delete request handler
+        [HttpDelete("{id}")]
+        public ActionResult Delete(Guid id)
+        {
+            _contactService.DeleteContact(id);
+            return NoContent();
+        }
+    }
+}
--- backend/Program.cs
+++ backend/Program.cs
@@ -1,6 +1,18 @@
+using API.Data;
+using API.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+// Add the repository as a singleton service
+builder.Services.AddSingleton<JsonRepository>();
+
+// Add the service as a scoped service
+builder.Services.AddScoped<IContactService, JsonContactService>();
+
+// Add AutoMapper
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -15,6 +27,10 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+/// frontend => localhost:3000
+// add a CORS policy to accept http requests from localhost:3000
+app.UseCors(options => options.WithOrigins("http://localhost:3000").AllowAnyMethod().AllowAnyHeader());
 
 app.UseAuthorization();
 
