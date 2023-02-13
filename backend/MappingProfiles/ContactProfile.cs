c2e5cb267a727fb6c9038a74ab8dd145bcf1eab2
Vincent Nadal <vnadal@fullscale.io>
2023-02-13 14:16:45 +0800

    Add service layer

---
+++ backend/MappingProfiles/ContactProfile.cs
@@ -0,0 +1,19 @@
+﻿using API.DTOs;
+using API.Models;
+using AutoMapper;
+
+namespace API.MappingProfiles
+{
+    public class ContactProfile : Profile
+    {
+        // Use AutoMapper to map the DTOs to the Models and for CreateContactDto generate an ID when it gets converted to the Contact class
+        public ContactProfile()
+        {
+            CreateMap<Contact, ReadContactDTO>().ReverseMap();
+            CreateMap<CreateContactDTO, Contact>()
+                .ForMember(dest => dest.Id, opt => opt.MapFrom(src => Guid.NewGuid()))
+                .ReverseMap();
+            CreateMap<UpdateContactDTO, Contact>().ReverseMap();
+        }
+    }
+}
---
+++ backend/Services/IContactService.cs
@@ -0,0 +1,14 @@
+﻿using API.DTOs;
+
+namespace API.Services
+{
+    public interface IContactService
+    {
+        // Use JsonRepository's Crud Methods 
+        List<ReadContactDTO> GetAllContacts();
+        ReadContactDTO GetContact(Guid id);
+        ReadContactDTO AddContact(CreateContactDTO contact);
+        ReadContactDTO UpdateContact(UpdateContactDTO contact);
+        void DeleteContact(Guid id);
+    }
+}
